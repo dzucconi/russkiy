77b3de385bda8ca92fc09be8b4ef85055126d720
dzucconi <mail@damonzucconi.com>
2020-09-07 14:06:34 -0400

    Adds a schedule

--- index.ts
+++ index.ts
@@ -14,21 +14,42 @@ export const remap = (
   );
 };
 
-const el = document.getElementById("root");
-
 const RUNTIME_IN_HOURS = 2;
 const WORDS_PER_HOUR = words.length / RUNTIME_IN_HOURS;
 const WORDS_PER_MINUTE = WORDS_PER_HOUR / 60;
 const WORDS_PER_SECOND = WORDS_PER_MINUTE / 60;
 
-const _INDEX: { [letter: string]: number } = words.reduce((memo, word, i) => {
-  const letter = word.charAt(0);
+const init = () => {
+  const el = document.getElementById("root");
+  const date = new Date();
+  const now =
+    date.getHours() + date.getMinutes() / 60 + date.getSeconds() / 60 / 60;
+  const interval = now % 2;
+  const offset = Math.floor(remap(interval, 0, 2, 0, words.length));
 
-  if (typeof memo[letter] === "number") {
+  const fi = new FrameInterval(WORDS_PER_SECOND, ({ frame }) => {
+    el.innerHTML = words[(frame + offset) % words.length];
+  });
+
+  fi.start();
+};
+
+init();
+
+type Index = {
+  [letter: string]: {
+    index: number;
+    interval: number;
+  };
+};
+
+const INDEX = words.reduce((memo: Index, word, i) => {
+  const letter = word.charAt(0);
+  if (!!memo[letter]) {
     return memo;
   }
 
-  memo[letter] = i;
+  memo[letter] = { index: i, interval: remap(i, 0, words.length, 0, 2) };
   return memo;
 }, {});
 
@@ -40,14 +61,22 @@ console.info(
   } hours.`
 );
 
-const date = new Date();
-const now =
-  date.getHours() + date.getMinutes() / 60 + date.getSeconds() / 60 / 60;
-const interval = now % 2;
-const offset = Math.floor(remap(interval, 0, 2, 0, words.length));
+const pad = (n: number): string => (n < 10 ? `0${n}` : `${n}`);
 
-const fi = new FrameInterval(WORDS_PER_SECOND, ({ frame }) => {
-  el.innerHTML = words[(frame + offset) % words.length];
+const time = (dt: number) => {
+  dt = dt * 60 * 60;
+  const hours = Math.floor(dt / (60 * 60));
+  dt = dt - hours * 60 * 60;
+  const minutes = Math.floor(dt / 60);
+  dt = dt - minutes * 60;
+  const seconds = Math.round(dt);
+  return [hours, minutes, seconds].map(pad).join(":");
+};
+
+const SCHEDULE = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22].map((offset) => {
+  Object.keys(INDEX).map((letter) => {
+    console.info(`| ${letter} | ${time(INDEX[letter].interval + offset)} |`);
+  });
 });
 
-fi.start();
+console.info(SCHEDULE);
